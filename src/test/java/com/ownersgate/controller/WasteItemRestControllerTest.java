a74ebe06cc39d446527fc46b0bccb9ae09e38240
rock020499-ops <rock020499@gmail.com>
2026-08-16 13:04:55 +0900

    テストコード修正

--- src/test/java/com/ownersgate/controller/WasteItemRestControllerTest.java
+++ src/test/java/com/ownersgate/controller/WasteItemRestControllerTest.java
@@ -8,14 +8,19 @@ import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
+import com.ownersgate.config.SecurityConfig;
+import com.ownersgate.service.AppUserDetailsService;
 import com.ownersgate.service.WasteItemService;
 
-@WebMvcTest(WasteItemRestController.class)
+@WebMvcTest(value = WasteItemRestController.class, excludeAutoConfiguration = UserDetailsServiceAutoConfiguration.class)
+@Import(SecurityConfig.class)
 class WasteItemRestControllerTest {
 
     @Autowired
@@ -23,6 +28,10 @@ class WasteItemRestControllerTest {
 
     @MockitoBean
     private WasteItemService service;
+
+    // SecurityConfig が必要とする UserDetailsService をモック化して提供する
+    @MockitoBean
+    private AppUserDetailsService appUserDetailsService;
 
     @Test
     @WithMockUser
